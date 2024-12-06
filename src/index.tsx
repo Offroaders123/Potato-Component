80e982acfcbdb73a1e2163c57903ef279c737ca6
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-12-06 04:48:02 -0800

    Component Class Type

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import { render } from "solid-js/web";
 
 import type { Component } from "solid-js";
 
-export declare class ComponentClass<P extends Record<string, any>> extends HTMLElement {
+declare class ComponentClass<P extends Record<string, any>> extends HTMLElement {
   constructor(props: P);
 }
+
+export type { ComponentClass };
 
 export function createComponent<K extends keyof HTMLElementTagNameMap, P extends Record<string, any>>(name: K, component: Component<P>): typeof ComponentClass<P> {
   const Component: Component<P> = component;
